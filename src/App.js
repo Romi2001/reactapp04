9dd80ff010e8e83236d3b3f718ef0b0b0506bca7
Yessenia <yesseniahuillca07@gmail.com>
2023-08-04 09:17:38 -0500

    numeros

--- src/App.js
+++ src/App.js
@@ -11,26 +11,15 @@ import C07MatrizOperaciones from './component/C07MatrizOperaciones';
 function App() {
   return (
     <div >
-      <h1>componete 01</h1>
-     <C01componente/>
-
-     <h1>componente 02</h1>
-     <C02contador/>
-
-     <h1>componente 03</h1>
-     <C03dobleestado/>
+     <h1>componente 06</h1>
+     <C06matriz/>
 
      <h1>componente 04</h1>
      <C04variable xVariable="hola mundo..." />
 
-     <h1>componente 05</h1>
-     <C05operador />
-
-     <h1>componente 06</h1>
-     <C06matriz/>
-
-     <h1>componente 07</h1>
-     <C07MatrizOperaciones/>
+    <h1>componente 02</h1>
+     <C02contador/>
+  
     </div>
   );
 }
